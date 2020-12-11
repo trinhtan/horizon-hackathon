c36e95594b3d47220f51734dd3d4a6532215852c
tranchien <chientm@apples-MacBook-Pro.local>
2020-12-11 10:03:58 +0700

    add store + redux dev tool

---
+++ client/src/store/hmy/reducer.js
@@ -0,0 +1,11 @@
+import * as actions from './action';
+const initState = {};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export { reducer as hmyReducer };
