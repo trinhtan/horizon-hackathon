2ee7f47e6c4d53d7dd4122bb298662337ab12a6f
trinhtan <lop12a1trinhvantan@gmail.com>
2020-12-15 13:14:11 +0700

    init relayer

---
+++ cmd/ebd/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/cli"
+	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/trinhtan/horizon-hackathon/app"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+)
+
+func main() {
+	cdc := app.MakeCodec()
+
+	// TODO: set custom bech32 prefixes for peggy
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount(sdk.Bech32PrefixAccAddr, sdk.Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(sdk.Bech32PrefixValAddr, sdk.Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
+	config.Seal()
+
+	ctx := server.NewDefaultContext()
+	cobra.EnableCommandSorting = false
+
+	rootCmd := &cobra.Command{
+		Use:               "ebd",
+		Short:             "Ethereum Bridge App Daemon (server)",
+		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
+	}
+
+	rootCmd.AddCommand(genutilcli.InitCmd(ctx, cdc, app.ModuleBasics, app.DefaultNodeHome))
+	rootCmd.AddCommand(genutilcli.CollectGenTxsCmd(ctx, cdc, auth.GenesisAccountIterator{}, app.DefaultNodeHome))
+	rootCmd.AddCommand(genutilcli.GenTxCmd(ctx, cdc, app.ModuleBasics, staking.AppModuleBasic{},
+		auth.GenesisAccountIterator{}, app.DefaultNodeHome, app.DefaultCLIHome))
+	rootCmd.AddCommand(genutilcli.ValidateGenesisCmd(ctx, cdc, app.ModuleBasics))
+	rootCmd.AddCommand(AddGenesisAccountCmd(ctx, cdc, app.DefaultNodeHome, app.DefaultCLIHome))
+
+	server.AddCommands(ctx, cdc, rootCmd, newApp, exportAppStateAndTMValidators)
+
+	// prepare and add flags
+	executor := cli.PrepareBaseCmd(rootCmd, "EB", app.DefaultNodeHome)
+	if err := executor.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application {
+	return app.NewEthereumBridgeApp(
+		logger, db, true,
+		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
+		baseapp.SetHaltHeight(uint64(viper.GetInt(server.FlagHaltHeight))),
+	)
+}
+
+func exportAppStateAndTMValidators(
+	logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailWhiteList []string,
+) (json.RawMessage, []tmtypes.GenesisValidator, error) {
+
+	if height != -1 {
+		ebApp := app.NewEthereumBridgeApp(logger, db, false)
+		if err := ebApp.LoadHeight(height); err != nil {
+			return nil, nil, err
+		}
+		return ebApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+	}
+	ebApp := app.NewEthereumBridgeApp(logger, db, true)
+	return ebApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+}
---
+++ go.mod
@@ -0,0 +1,36 @@
+module github.com/trinhtan/horizon-hackthon
+
+go 1.13
+
+require (
+	github.com/allegro/bigcache v1.2.0 // indirect
+	github.com/aristanetworks/goarista v0.0.0-20190528200627-2e9fd846018e // indirect
+	github.com/cespare/cp v1.1.1 // indirect
+	github.com/cosmos/cosmos-sdk v0.39.1
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
+	github.com/etcd-io/bbolt v1.3.3 // indirect
+	github.com/ethereum/go-ethereum v1.8.27
+	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
+	github.com/gorilla/mux v1.7.4
+	github.com/huin/goupnp v1.0.0 // indirect
+	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/karalabe/hid v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/miguelmota/go-solidity-sha3 v0.1.0
+	github.com/onsi/ginkgo v1.9.0 // indirect
+	github.com/onsi/gomega v1.6.0 // indirect
+	github.com/pborman/uuid v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.6.3
+	github.com/stretchr/testify v1.6.1
+	github.com/tendermint/go-amino v0.15.1
+	github.com/tendermint/tendermint v0.33.7
+	github.com/tendermint/tm-db v0.5.1
+	github.com/yhat/ws v0.0.0-20150108175532-4c48c57eb28f // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+)
