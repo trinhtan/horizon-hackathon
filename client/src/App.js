79c6ffce9ec088a481f02ab8fd3f1d7bf9ae24f4
tranchien <chientm@apples-MacBook-Pro.local>
2020-12-25 11:30:47 +0700

    feat: add token + oracle

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import { useEffect } from 'react';
+// import { connectOneWallet } from './utils/connectOneWallet';
+// import { connectMetamask } from './utils/connectMetamask';
+// import { convertToken } from 'utils/getTokenInfo';
 import { Layout } from 'antd';
 
 import Head from 'components/Head';
@@ -8,6 +12,16 @@ import Footer from 'components/Footer';
 import SwapPage from 'pages/SwapPage';
 
 function App() {
+  // useEffect(() => {
+  //   // connectOneWallet();
+  //   // connectMetamask();
+  //   let test = async () => {
+  //     let price = await convertToken('BAND', 'USD', 100);
+  //     console.log('price', price);
+  //   };
+  //   test();
+  // });
+
   return (
     <Router>
       <Layout className='App'>
